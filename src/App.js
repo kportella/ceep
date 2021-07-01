b90d75e27e4fd1a2c045963a8c22b9ab1712c785
Kaue Portella <portellakaue@gmail.com>
2021-06-30 23:02:17 -0300

    Change something

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { Component } from "react";
-import FormularioCadastro from "./components/FormularioCadastro.jsx";
-import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas.jsx";
-
-
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
   render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
-      </section >
+      </section>
     );
   }
 }
+
 export default App;
